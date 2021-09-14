1d6bdd4b3186ada921688abdfce8ddee6817bd0e
Trace <tracedelange@me.com>
2021-09-14 15:30:21 -0500

    commit tree 2f7d79bea76097bf30c954de1134b2b7d231dcee

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,7 @@
 class Post < ApplicationRecord
+
+    validates :title, presence: true
+    validates :category, inclusion: { in: %w(Non-Fiction Fiction)}
+    validates :content, length: {minimum: 100}
+    
 end
